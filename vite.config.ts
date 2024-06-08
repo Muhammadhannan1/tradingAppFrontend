59a4c27afd4e719784fdf87f30a89116e2f080cb
Muhammad Hannan <syedmhannan@gmail.com>
2024-06-08 17:47:48 +0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:6300},
+  server:{
+    host: '0.0.0.0', // Bind to all network interfaces
+    port:6300
+  },
   preview: {
+    host: '0.0.0.0', // Bind to all network interfaces
     port: 6300,
   },
 },
